6a95bb80a56743285b527703b588f97f76b57a0b
Anya Aven <leafeon96@gmail.com>
2024-08-20 17:38:22 -0400

    Default stories

---
+++ models/default-stories.ts
@@ -0,0 +1,31 @@
+
+export const defaultMadLibStories = {
+    "Excited Story": {
+        prompts: ["noun", "verb"],
+        text: `OMG!! OMG!! I love to [verb] a [noun]!`
+    },
+    "Scary Story": {
+        prompts: ["adjective1", "noun1", "verb1", "adjective2", "noun2"],
+        text: `In the dark, [adjective1] forest, there lurked a terrifying [noun1].
+    It would [verb1] with its [adjective2] [noun2], sending shivers down the
+    spine of anyone who dared to venture near.`
+    },
+    "Campfire Story": {
+        prompts: ["adjective1", "noun1", "verb1", "adjective2", "noun2"],
+        text: `Gather 'round the campfire, children, and listen closely to the tale
+     of the [adjective1] [noun1]. Legend has it that it would [verb1] with its
+     [adjective2] [noun2] under the light of the moon.`
+    },
+    "Swimming Pool Story": {
+        prompts: ["adjective1", "noun1", "verb1", "adjective2", "noun2"],
+        text: `As the sun set over the shimmering [noun1], a mysterious [adjective1]
+    figure emerged. It seemed to [verb1] with the grace of a [adjective2] [noun2],
+    captivating all who watched from the poolside.`
+    },
+    "Pets Story": {
+        prompts: ["adjective1", "noun1", "verb1", "adjective2", "noun2"],
+        text: `In the cozy corner of the house, there lived a peculiar [adjective1]
+    [noun1]. It would [verb1] with such [adjective2] [noun2], bringing joy to
+    all who knew its playful antics.`
+    }
+};
